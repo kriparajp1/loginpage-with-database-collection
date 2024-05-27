701afb9ed4dac70346f65a689b8e391b66a68404
kriparaj P <156789243+kriparajp1@users.noreply.github.com>
2024-05-27 13:01:21 +0530

    Update mongodb.js

--- models/mongodb.js
+++ models/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose =require("mongoose")
-mongoose.connect('mongodb://localhost:27017/userbase').then(()=>{
+mongoose.connect("mongodb+srv://kriparaj456:Cw3yMPqkrin266Fd@cluster0.9jdtsjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log('mongodb had connected');
 }).catch(()=>{
     console.log('mongodb has not connected');
